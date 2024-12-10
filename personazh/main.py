98adf110b3cb44c6c5d8e56964a68c313fb12366
Sasha Gusev <sasha.e.gusev3@yandex.ru>
2024-12-10 22:26:22 +0300

    creature

---
+++ personazh/main.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+import pygame
+
+# Изображение не получится загрузить
+# без предварительной инициализации pygame
+pygame.init()
+size = width, height = 300, 300
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+pygame.display.set_caption('Человечек')
+image = load_image("creature.png")
+x = 10
+y = 10
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x -= 10
+            elif event.key == pygame.K_RIGHT:
+                x += 10
+            elif event.key == pygame.K_UP:
+                y -= 10
+            elif event.key == pygame.K_DOWN:
+                y += 10
+    screen.fill((255, 255, 255))
+    screen.blit(image, (x, y))
+    pygame.display.flip()
